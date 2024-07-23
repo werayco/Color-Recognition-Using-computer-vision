2a7d3ad358cd559dbd9419b4b524184fe8db091b
Ray <142559582+werayco@users.noreply.github.com>
2024-07-23 03:13:07 -0700

    Update color.py

--- color.py
+++ color.py
@@ -1,15 +1,15 @@
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
-
+cap = cv.VideoCapture(0) # using the webcam for capturing data
 
 while True:
-    '''this code basically recognizes'''
+    '''this code basically recognizes the color of the centre pixel'''
     _,frame_bgr = cap.read()
-    frame_hsv = cv.cvtColor(frame_bgr,cv.COLOR_BGR2HSV)
-    cx,cy = int(frame_bgr.shape[1]//2),int(frame_bgr.shape[0]//2)
-    cnt = frame_hsv[cx,cy]
-    cv.circle(frame_bgr,(cx,cy),8,(0,255,0),1)
+    frame_hsv = cv.cvtColor(frame_bgr,cv.COLOR_BGR2HSV) # converting our frame to hsv channel
+    cx,cy = int(frame_bgr.shape[1]//2),int(frame_bgr.shape[0]//2) # finding the centre of the frame
+    cnt = frame_hsv[cx,cy] # highlighting the centre of the frame
+    cv.circle(frame_bgr,(cx,cy),8,(0,255,0),1) # creating a circle at the centre of the frame
+    
     hue = cnt[0]
     sat = cnt[1]
     val = cnt[2]
@@ -33,9 +33,10 @@ while True:
         cv.putText(frame_bgr,"Black",(100,100),0,1,(255,0,0),1)
     else:
         color=""
-    print(cnt)
-    # cv.putText(frame_bgr,color,(100,100),0,1,(255,0,0),1)
+        
+    print(cnt) # this returns an array of intensities of the centre pixel
     cv.imshow("frame",frame_bgr)
+    
     if cv.waitKey(1) & 0xFF == ord("x"):
         break
 
