8f50c72488b9ef956f6719d7425d10e6e086ce04
Ray <142559582+werayco@users.noreply.github.com>
2024-07-23 04:35:11 -0700

    Update color.py

--- color.py
+++ color.py
@@ -2,6 +2,12 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0) # using the webcam for capturing data
 
+switch = False
+def onClick(event,x,y,flags,params):
+    global switch
+    if event == cv.EVENT_LBUTTONDOWN:
+        switch = not switch
+        
 while True:
     '''this code basically recognizes the color of the centre pixel'''
     _,frame_bgr = cap.read()
@@ -15,28 +21,29 @@ while True:
     val = cnt[2]
 
 
-    if hue <5:
-        cv.putText(frame_bgr,"Red",(100,100),0,1,(255,0,0),1)
-    elif hue == 0:
-        cv.putText(frame_bgr,"Black",(100,100),0,1,(255,0,0),1)
-    elif hue<22:
-        cv.putText(frame_bgr,"Orange",(100,100),0,1,(255,0,0),1)
-    elif hue<33:
-        cv.putText(frame_bgr,"Yellow",(100,100),0,1,(255,0,0),1)
-    elif hue<78:
-        cv.putText(frame_bgr,"Green",(100,100),0,1,(255,0,0),1)
-    elif hue<131:
-        cv.putText(frame_bgr,"Blue",(100,100),0,1,(255,0,0),1)
-    elif hue<170:
-        cv.putText(frame_bgr,"Violet",(100,100),0,1,(255,0,0),1)
-    elif hue ==0 and sat == 0 and val == 0:
-        cv.putText(frame_bgr,"Black",(100,100),0,1,(255,0,0),1)
+if switch: # Using the switch as a flag
+        if hue <5:
+            cv.putText(frame_bgr,"Red",(100,100),0,1,(255,0,0),1)
+        elif hue == 0:
+            cv.putText(frame_bgr,"Black",(100,100),0,1,(255,0,0),1)
+        elif hue<22:
+            cv.putText(frame_bgr,"Orange",(100,100),0,1,(255,0,0),1)
+        elif hue<33:
+            cv.putText(frame_bgr,"Yellow",(100,100),0,1,(255,0,0),1)
+        elif hue<78:
+            cv.putText(frame_bgr,"Green",(100,100),0,1,(255,0,0),1)
+        elif hue<131:
+            cv.putText(frame_bgr,"Blue",(100,100),0,1,(255,0,0),1)
+        elif hue<170:
+            cv.putText(frame_bgr,"Violet",(100,100),0,1,(255,0,0),1)
+        elif hue ==0 and sat == 0 and val == 0:
+            cv.putText(frame_bgr,"Black",(100,100),0,1,(255,0,0),1)
+        else:
+            color=""
+        print(cnt)
+        cv.imshow("frame",frame_bgr)
     else:
-        color=""
-        
-    print(cnt) # this returns an array of intensities of the centre pixel
-    cv.imshow("frame",frame_bgr)
-    
+        cv.imshow("frame",frame_bgr)
     if cv.waitKey(1) & 0xFF == ord("x"):
         break
 
